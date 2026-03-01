efef9c3ec8918a3eaa0bf0344a30fc354e572108
GabrielGame311 <83588152+GabrielGame311@users.noreply.github.com>
2026-03-01 01:34:43 +0100

    fixed skydriving freefall character controller

---
+++ Assets/SkydivingController.cs
@@ -0,0 +1,7 @@
+using Cinemachine;
+using UnityEngine;
+
+public class SkydivingController : MonoBehaviour
+{
+   
+}
--- Assets/freefall.cs
+++ Assets/freefall.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Cinemachine;
+
+
 
 public class freefall : MonoBehaviour
 {
-
+    [Header("Components")]
     public CharacterController controller;
-    Vector3 falldown;
-    public float MovingSpeed;
+    private RatchetController ratchet; // Cacha denna för prestanda
+    private Animator anim;
+
+    [Header("Camera Settings")]
     public GameObject CamFreefall;
-    public float falldawnSpeed;
-    public float MoveSpeed = 8;
+    private CinemachineVirtualCamera vCam; // Den faktiska kamerakomponenten
+
+    [Header("Fall Settings")]
+    public float falldawnSpeed = 20f;
+    public float MoveSpeed = 8f;
     public bool ItsFalling = false;
+    
     public static freefall Freefall;
     public bool IsMoving = false;
     public bool IsScene = false;
@@ -23,100 +32,140 @@ public class freefall : MonoBehaviour
     public float hInput;
     public float vInput;
     float GravityStart;
+
+   
+
     private void Start()
     {
 
         Freefall = GetComponent<freefall>();
         controller = GetComponent<CharacterController>();
-        GravityStart = GetComponent<RatchetController>().Gravity;
+        
+        ratchet = GetComponent<RatchetController>();
+        anim = GetComponent<Animator>();
+
+        // Hämta Cinemachine-komponenten från ditt CamFreefall objekt
+        if (CamFreefall != null)
+            vCam = CamFreefall.GetComponent<CinemachineVirtualCamera>();
+
+        GravityStart = ratchet.Gravity;
 
     }
 
     private void Update()
     {
-
-        
-
-        if(ItsFalling)
+        if (ItsFalling)
         {
-           
-           // IsMoving = true;
-            GetComponent<RatchetController>().anime.SetBool("FreeFall", true);
-            CamFreefall.SetActive(true);
-           
-           // Camera.main.gameObject.SetActive(false);
-            GetComponent<RatchetController>().CanMove = false;
-            controller.Move(Vector3.down * falldawnSpeed * Time.deltaTime);
-
-
-             hInput = Input.GetAxis("Horizontal");
-             vInput = Input.GetAxis("Vertical");
-            if (IOSController.IosController_ != null)
-            {
-
-                hInput = IOSController.IosController_.JoyStick_.Horizontal;
-                vInput = IOSController.IosController_.JoyStick_.Vertical;
-            }
-          
-            if(PlayMusic)
-            {
-                sound.gameObject.SetActive(true);
-                MusicPlayed.gameObject.SetActive(false);
-            }
-
-
-
-           pos = new Vector3(hInput, 0, vInput);
-
-            // Move the player based on input
-            controller.Move(pos * MoveSpeed * Time.deltaTime);
-            RatchetController.RatchetController_.enabled = false;
-
-
+            HandleFalling();
         }
         else
         {
-            RatchetController.RatchetController_.enabled = true;
-            GetComponent<RatchetController>().CanMove = true;
-            CamFreefall.SetActive(false);
-            GetComponent<RatchetController>().anime.SetBool("FreeFall", false);
-            
-            //Camera.main.gameObject.SetActive(true);
-        }
-      
-
-        if(IsScene)
-        {
-            if(EnemiesMission.instance.Mission == 0)
-            {
-                if (IsMoving)
-                {
-                    GetComponent<Animator>().applyRootMotion = true;
-                }
-                else
-                {
-                    GetComponent<Animator>().applyRootMotion = false;
-                }
-            }
-               
-            
-        }
-        else
-        {
-            GetComponent<Animator>().applyRootMotion = true;
+            HandleNormalState();
         }
 
-       
+        HandleRootMotion();
 
-        if(controller.isGrounded)
+        if (controller.isGrounded && ItsFalling)
         {
-            ItsFalling = false;
-            IsMoving = false;
-            GetComponent<RatchetController>().Gravity = GravityStart;
-            
+            StopFalling();
         }
     }
 
+    void HandleFalling()
+    {
+        anim.SetBool("FreeFall", true);
+        CamFreefall.SetActive(true);
+        ratchet.CanMove = false;
+        ratchet.enabled = false;
+
+        // 1. Rörelse nedåt
+        controller.Move(Vector3.down * falldawnSpeed * Time.deltaTime);
+
+        // 2. Rörelse i sidled (Styrning)
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+
+        if (IOSController.IosController_ != null)
+        {
+            hInput = IOSController.IosController_.JoyStick_.Horizontal;
+            vInput = IOSController.IosController_.JoyStick_.Vertical;
+        }
+
+        pos = new Vector3(hInput, 0, vInput);
+        controller.Move(pos * MoveSpeed * Time.deltaTime);
+
+        // 3. Dynamisk FOV (Fartkänsla!)
+        if (vCam != null)
+        {
+            // Vi baserar FOV på falldawnSpeed. Ju snabbare fall, desto mer FOV.
+            // Här mappar vi hastigheten (t.ex. 20) till ett FOV mellan 60 och 90.
+            float targetFOV = Mathf.Lerp(60, 90, falldawnSpeed / 50f);
+            vCam.m_Lens.FieldOfView = Mathf.Lerp(vCam.m_Lens.FieldOfView, targetFOV, Time.deltaTime * 2f);
+        }
+
+        // Ljudhantering
+        if (PlayMusic && sound != null)
+        {
+            sound.gameObject.SetActive(true);
+            MusicPlayed.gameObject.SetActive(false);
+        }
+    }
+
+    void StopFalling()
+    {
+        ItsFalling = false;
+        IsMoving = false;
+        ratchet.Gravity = GravityStart;
+        ratchet.enabled = true;
+        ratchet.CanMove = true;
+        CamFreefall.SetActive(false);
+        anim.SetBool("FreeFall", false);
+    }
+    // Hanterar vad som händer när vi INTE faller
+    void HandleNormalState()
+    {
+        // Säkerställ att vi inte råkar ha kvar fall-inställningar
+        if (ratchet != null)
+        {
+            ratchet.enabled = true;
+            ratchet.CanMove = true;
+        }
+
+        if (CamFreefall != null && CamFreefall.activeSelf)
+        {
+            CamFreefall.SetActive(false);
+        }
+
+        if (anim != null)
+        {
+            anim.SetBool("FreeFall", false);
+        }
+
+        // Stäng av vindljudet om det körs
+        if (sound != null && sound.gameObject.activeSelf)
+        {
+            sound.gameObject.SetActive(false);
+            MusicPlayed.gameObject.SetActive(true);
+        }
+    }
+
+    // Hanterar Root Motion baserat på om det är en cutscene eller inte
+    void HandleRootMotion()
+    {
+        if (IsScene)
+        {
+            // Om vi är i en specifik mission-scen (från ditt originalskript)
+            if (EnemiesMission.instance != null && EnemiesMission.instance.Mission == 0)
+            {
+                anim.applyRootMotion = IsMoving;
+            }
+        }
+        else
+        {
+            // Standardläge: Root Motion är på för normal rörelse
+            anim.applyRootMotion = true;
+        }
+    }
 
     public void RunForward()
     {
@@ -133,5 +182,6 @@ public class freefall : MonoBehaviour
 
     }
 
+    
 
 }
