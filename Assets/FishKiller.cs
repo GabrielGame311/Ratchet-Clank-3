18752e70108e123eff67ead779ceedb32bdea1b2
GabrielGame311 <83588152+GabrielGame311@users.noreply.github.com>
2026-05-06 16:10:58 +0200

    fish killer

---
+++ Assets/FishKiller.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+
+public class FishKiller : MonoBehaviour
+{
+
+    Animator anime;
+
+    public float Impuls;
+    public float rotate;
+    Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anime = GetComponentInChildren<Animator>();
+        StartCoroutine(wait());
+        rb.AddForce(transform.forward * Impuls);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Ratchet")
+        {
+
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator wait()
+    {
+
+        yield return new WaitForSeconds(0);
+
+        anime.SetTrigger("Eat");
+        GameObject.FindObjectOfType<Player>().TakeDamage(250);
+        GameObject.FindObjectOfType<Player>().GetComponent<CharacterController>().enabled = false;
+        yield return new WaitForSeconds(1);
+        transform.Rotate(rotate, 0, 0);
+       
+        yield return new WaitForSeconds(1);
+        
+        Destroy(gameObject);
+
+    }
+}
--- Assets/Water.cs
+++ Assets/Water.cs
@@ -8,22 +8,60 @@ public class Water : MonoBehaviour
 
     public Animator anime;
 
+    public GameObject Fish_;
+
+    public float count;
+    public GameObject player;
+    public bool isTrigger = false;
 
     private void OnTriggerEnter(Collider other)
     {
      
-        if(other.tag =="Player") 
+        if(other.tag == "Armor") 
         {
-            waterscreen.SetActive(true);
 
-            
-            
-            
+            //waterscreen.SetActive(true);
+
+            isTrigger = true;
+
+
         }
         
     }
 
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Armor")
+        {
+
+            isTrigger = false;
+
+
+
+
+
+
+        }
+    }
+
    
 
+    private void Update()
+    {
+        if(isTrigger)
+        {
+
+            count -= Time.deltaTime;
+            if(count < 0)
+            {
+
+                count = 1000;
+                Instantiate(Fish_, player.transform.position, player.transform.rotation);
+                isTrigger = false;
+            }
+        }
+    }
 
 }
